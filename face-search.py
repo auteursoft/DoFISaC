f1a9356833330410e152c0aea1e76ebd3aa0fccc
Sean P. Goggins <s@goggins.com>
2025-05-05 08:14:09 -0500

    updated image display
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- face-search.py
+++ face-search.py
@@ -1,5 +1,6 @@
 import face_recognition
 import pickle
+import os
 
 # Load the index
 with open("face_index.pkl", "rb") as f:
@@ -9,6 +10,7 @@ with open("face_index.pkl", "rb") as f:
 query_image = face_recognition.load_image_file("query.jpg")
 query_encodings = face_recognition.face_encodings(query_image)
 
+# Match results
 results = []
 for query_encoding in query_encodings:
     for entry in face_db:
@@ -16,4 +18,21 @@ for query_encoding in query_encodings:
         if match[0]:
             results.append(entry["path"])
 
-print("Found matches:", results)
+# Generate HTML (referencing image files on disk)
+html = ['<html><head><title>Face Match Results</title></head><body>']
+html.append('<h1>Matched Images</h1>')
+
+if results:
+    for path in results:
+        rel_path = os.path.relpath(path)  # ensures relative path from current dir
+        html.append(f'<div><p>{rel_path}</p><img src="{rel_path}" style="max-width: 300px;"><br><br></div>')
+else:
+    html.append('<p>No matches found.</p>')
+
+html.append('</body></html>')
+
+# Write to HTML file
+with open("matches.html", "w") as f:
+    f.write('\n'.join(html))
+
+print("HTML file with results saved to matches.html")
